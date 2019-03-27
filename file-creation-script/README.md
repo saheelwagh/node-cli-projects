f2f8b63a690eeb48aee1eb8f2082149ec0515e02
Saheel Wagh <saheelwagh@Gmail.com>
2019-03-27 10:10:05 +0530

    setup readme and created index shell

---
+++ file-creation-script/README.md
@@ -0,0 +1,5 @@
+# Tuotorial on [Medium.com](https://medium.com) by Hugo Dias
+
+_<https://codeburst.io/building-a-node-js-interactive-cli-3cb80ed76c86>_
+
+This CLI asks some questions and creates a file based on the answers.
---
+++ file-creation-script/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+//import inquirer, chalk, figlet and sehll 
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const shell = require('shelljs');
+
+const run = async () => {
+    /*
+     *introduce the script
+     *ask questions
+     *generate the script
+     */
+
+}
